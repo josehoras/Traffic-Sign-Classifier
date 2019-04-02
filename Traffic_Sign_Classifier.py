088916a88a8aabe63e73198c8eb96df1effcc22f
josehoras <jose.horas@gmail.com>
2019-04-02 19:00:06 +0200

    Clean up

--- Traffic_Sign_Classifier.py
+++ Traffic_Sign_Classifier.py
@@ -1,8 +1,16 @@
 ### Step 0: Load The Data
-# Load pickled data
+
+import tensorflow as tf
+from tensorflow.contrib.layers import flatten
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+import csv
 import pickle
 import numpy as np
+from PIL import Image
+import os
 
+# Load pickled data
 training_file = "train.p"
 validation_file = "valid.p"
 testing_file = "test.p"
@@ -14,9 +22,9 @@ with open(validation_file, mode='rb') as f:
 with open(testing_file, mode='rb') as f:
     test = pickle.load(f)
 
-X_train_raw, y_train_raw = train['features'], train['labels']
-X_valid_raw, y_valid = valid['features'], valid['labels']
-X_test_raw, y_test = test['features'], test['labels']
+X_train, y_train = train['features'], train['labels']
+X_valid, y_valid = valid['features'], valid['labels']
+X_test, y_test = test['features'], test['labels']
 
 
 # Add augmented training data
@@ -30,20 +38,8 @@ def add_data(new_data_file, X_train_old, y_train_old):
     return X_train_old, y_train_old
 
 
-X_train_aug, y_train_aug = add_data("train_rot.p", X_train_raw, y_train_raw)
-X_train_aug, y_train_aug = add_data("train_rect.p", X_train_aug, y_train_aug)
-X_train_aug, y_train_aug = add_data("train_trans.p", X_train_aug, y_train_aug)
-
-# preprocess to gray
-def prepro(x):
-    x = x.astype('float32')
-    x = (x - 128.) / 128.
-    return x
-
-X_train = prepro(X_train_aug)
-y_train = y_train_aug
-X_valid = prepro(X_valid_raw)
-X_test = prepro(X_test_raw)
+for filename in ("train_trans.p", "train_rot.p", "train_rect.p"):
+    X_train, y_train = add_data(filename, X_train, y_train)
 
 ### Step 1: Dataset Summary & Exploration
 ### Replace each question mark with the appropriate value.
@@ -70,24 +66,18 @@ print("Labels: ", y_train.shape, y_train[0])
 ### Feel free to use as many code cells as needed.
 
 # Open csv and place signnames in dictionary
-import csv
 reader = csv.reader(open('signnames.csv', mode='r'))
 signs_dict = dict((rows[0], rows[1]) for rows in reader)
 
-import matplotlib.pyplot as plt
-# Visualizations will be shown in the notebook.
-# n = 19000 + 34799
-# plt.imshow(X_train_aug[n])
-# plt.axis('off')
-# plt.title(signs_dict[str(y_train_aug[n])])
 
+# Visualizations will be shown in the notebook.
 plt.figure(1, figsize=(18, 7))
 for i in range(43):
     plt.subplot(5, 9, i + 1)  # sets the number of feature maps to show on each row and column
     plt.title(str(i) + ": " + signs_dict[str(i)], fontsize=8)
     plt.axis('off')
-    idx = np.where(y_train_raw == i)[0][80]
-    plt.imshow(X_train_raw[idx])
+    idx = np.where(y_train == i)[0][80]
+    plt.imshow(X_train[idx])
 plt.tight_layout()
 plt.show()
 
@@ -95,64 +85,26 @@ plt.show()
 ### Preprocess the data here. It is required to normalize the data. Other preprocessing steps could include
 ### converting to grayscale, etc.
 ### Feel free to use as many code cells as needed.
-# Normalize the data: subtract the mean image
-# mean_image = np.mean(X_train, axis=0, dtype='uint8')
-# X_train -= mean_image
-# X_valid -= mean_image
-# X_test -= mean_image
+# preprocess to gray
+def prepro(x):
+    x = x.astype('float32')
+    x = (x - 128.) / 128.
+    return x
+
+X_train_norm = prepro(X_train)
+X_valid_norm = prepro(X_valid)
+X_test_norm = prepro(X_test)
 
 ### Define your architecture here.
 ### Feel free to use as many code cells as needed.
-import tensorflow as tf
-from tensorflow.contrib.layers import flatten
-
-
-def LeNet5(x):
-    # Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
-    conv1_weights = tf.get_variable("Wconv1", shape=[5, 5, 3, 6])
-    conv1_bias = tf.get_variable("bconv1", shape=[6])
-    conv1_layer = tf.nn.conv2d(x, conv1_weights, [1, 1, 1, 1], 'VALID') + conv1_bias
-        # Activation.
-    conv1_layer = tf.nn.relu(conv1_layer)
-        # Pooling. Input = 28x28x6. Output = 14x14x6.
-    pool1_layer = tf.nn.max_pool(conv1_layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-    # Layer 2: Convolutional. Input = 14x14x6. Output = 10x10x16.
-    conv2_weights = tf.get_variable("Wconv2", shape=[5, 5, 6, 16])
-    conv2_bias = tf.get_variable("bconv2", shape=[16])
-    conv2_layer = tf.nn.conv2d(pool1_layer, conv2_weights, [1, 1, 1, 1], 'VALID') + conv2_bias
-        # Activation.
-    conv2_layer = tf.nn.relu(conv2_layer)
-        # Pooling. Input = 10x10x16. Output = 5x5x16.
-    pool2_layer = tf.nn.max_pool(conv2_layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-    # Flatten. Input = 5x5x16. Output = 400.
-    flat = flatten(pool2_layer)
-    # Layer 3: Fully Connected. Input = 400. Output = 120
-    fc1_weights = tf.get_variable("W1", shape=[400, 120])
-    fc1_bias = tf.get_variable("b1", shape=[120])
-    fc1_layer = tf.add(tf.matmul(flat, fc1_weights), fc1_bias)
-        # Activation.
-    fc1_layer = tf.nn.relu(fc1_layer)
-    # Layer 4: Fully Connected. Input = 120. Output = 84
-    fc2_weights = tf.get_variable("W2", shape=[120, 84])
-    fc2_bias = tf.get_variable("b2", shape=[84])
-    fc2_layer = tf.add(tf.matmul(fc1_layer, fc2_weights), fc2_bias)
-        # Activation.
-    fc2_layer = tf.nn.relu(fc2_layer)
-    # Layer 5: Fully Connected. Input = 84. Output = 43
-    out_weights = tf.get_variable("Wout", shape=[84, 43])
-    out_bias = tf.get_variable("bout", shape=[43])
-    logits = tf.add(tf.matmul(fc2_layer, out_weights), out_bias)
-    return logits
-
-
-def LeNet_down(x):
+def sign_model(x):
     c1_out = 32
     c2_out = 64
     fc1_in = 5 * 5 * c2_out
     fc1_out = 1024
     fc2_out = 344
     fc3_out = 43
-    # Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
+    # Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x32.
     conv1_weights = tf.get_variable("Wconv1", shape=[5, 5, 3, c1_out])
     conv1_bias = tf.get_variable("bconv1", shape=[c1_out])
     conv1_layer = tf.nn.conv2d(x, conv1_weights, [1, 1, 1, 1], 'VALID') + conv1_bias
@@ -163,7 +115,7 @@ def LeNet_down(x):
     gamma = tf.get_variable("gamma", shape=[c1_out])
     mean, variance = tf.nn.moments(conv1_layer, axes=[0, 1, 2])
     conv1_layer = tf.nn.batch_normalization(conv1_layer, mean, variance, beta, gamma, 1e-8)
-        # Pooling. Input = 28x28x6. Output = 14x14x6.
+        # Pooling. Input = 28x28x6. Output = 14x14x64.
     pool1_layer = tf.nn.max_pool(conv1_layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
     # Layer 2: Convolutional. Input = 14x14x6. Output = 10x10x16.
     conv2_weights = tf.get_variable("Wconv2", shape=[5, 5, c1_out, c2_out])
@@ -196,36 +148,6 @@ def LeNet_down(x):
     return logits, conv1_layer
 
 
-def my_model(x):
-    # Layer 1: Convolutional. Input = 32x32x1. Output = 26x26x32
-    conv1_weights = tf.get_variable("Wconv1", shape=[7, 7, 1, 32])
-    conv1_bias = tf.get_variable("bconv1", shape=[32])
-    conv1_layer = tf.nn.conv2d(x, conv1_weights, strides=[1, 1, 1, 1], padding='VALID') + conv1_bias
-        # Activation
-    conv1_layer = tf.nn.relu(conv1_layer)
-        # Batch normalization
-        # beta = tf.get_variable("beta", shape=[32])
-        # gamma = tf.get_variable("gamma", shape=[32])
-        # mean, variance = tf.nn.moments(h1, axes=[0, 1, 2])
-        # bn = tf.nn.batch_normalization(h1, mean, variance, beta, gamma, 1e-8)
-        # Pooling. Input = 26x26x32. Output = 13x13x32
-    pool1_layer = tf.nn.max_pool(conv1_layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-    # Flatten.Input = 13x13x32.Output = 5408
-    flat_dim = 5408
-    flat = tf.reshape(pool1_layer, [-1, flat_dim])
-    # Layer 2: Fully Connected. Input = 5408. Output = 1024
-    fc1_weights = tf.get_variable("W1", shape=[flat_dim, 1024])
-    fc1_bias = tf.get_variable("b1", shape=[1024])
-    fc1_layer = tf.matmul(flat, fc1_weights) + fc1_bias
-        # Activation
-    fc1_layer = tf.nn.relu(fc1_layer)
-    # Layer 3: Fully Connected. Input = 1024. Output = 43
-    out_weights = tf.get_variable("W2", shape=[1024, 43])
-    out_bias = tf.get_variable("b2", shape=[43])
-    logits = tf.matmul(fc1_layer, out_weights) + out_bias
-    return logits
-
-
 ### Train your model here.
 ### Calculate and report the accuracy on the training and validation set.
 ### Once a final model architecture is selected,
@@ -238,7 +160,7 @@ keep_prob = tf.placeholder(tf.float32)
 
 ### training pipeline
 lr = tf.placeholder(tf.float32, shape=[])
-logits, act_layer = LeNet_down(x)
+logits, act_layer = sign_model(x)
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
 loss_operation = tf.reduce_mean(cross_entropy)
 optimizer = tf.train.AdamOptimizer(learning_rate=lr)
@@ -264,13 +186,12 @@ def evaluate(X_data, y_data):
 
 
 ### Train the model
-from sklearn.utils import shuffle
-EPOCHS = 20
+EPOCHS = 10
 BATCH_SIZE = 256
 rate = 1e-3
 rate_decay = 0.96
 
-batch_x, batch_y = X_train[0:BATCH_SIZE], y_train[0:BATCH_SIZE]
+batch_x, batch_y = X_train_norm[0:BATCH_SIZE], y_train[0:BATCH_SIZE]
 iterations = 0
 loss_series = []
 train_acc_series = []
@@ -290,10 +211,10 @@ if training:
         val_acc_series.append(validation_accuracy)
         acc_x.append(iterations)
         for i in range(EPOCHS):
-            X_train, y_train = shuffle(X_train, y_train)
+            X_train_norm, y_train = shuffle(X_train_norm, y_train)
             for offset in range(0, num_examples, BATCH_SIZE):
                 end = offset + BATCH_SIZE
-                batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+                batch_x, batch_y = X_train_norm[offset:end], y_train[offset:end]
                 loss, _ = sess.run([loss_operation, training_operation],
                                    feed_dict={x: batch_x, y: batch_y, keep_prob: 0.5, lr: rate})
                 loss_series.append(loss)
@@ -301,18 +222,18 @@ if training:
 
             # corr = sess.run(correct_prediction, feed_dict={x: batch_x, y: batch_y})
             # train_accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
-            # logit1 = sess.run(logits, feed_dict={x: X_train[0:1], y: y_train[0:1]})
-            # one_hot1 = sess.run(one_hot_y, feed_dict={x: X_train[0:1], y: y_train[0:1]})
+            # logit1 = sess.run(logits, feed_dict={x: X_train_norm[0:1], y: y_train[0:1]})
+            # one_hot1 = sess.run(one_hot_y, feed_dict={x: X_train_norm[0:1], y: y_train[0:1]})
             # print("Logit1: ", logit1)
             # print("One hot1: ", one_hot1)
-            # prediction = sess.run(pred, feed_dict={x: X_train[0:10], y: y_train[0:10]})
-            # labels = sess.run(label, feed_dict={x: X_train[0:10], y: y_train[0:10]})
+            # prediction = sess.run(pred, feed_dict={x: X_train_norm[0:10], y: y_train[0:10]})
+            # labels = sess.run(label, feed_dict={x: X_train_norm[0:10], y: y_train[0:10]})
             # print("Pred: ", prediction)
             # print("Label: ", labels)
 
-            train_accuracy = evaluate(batch_x, batch_y)
+            train_accuracy = evaluate(X_train_norm, y_train)
             train_acc_series.append(train_accuracy)
-            validation_accuracy = evaluate(X_valid, y_valid)
+            validation_accuracy = evaluate(X_valid_norm, y_valid)
             val_acc_series.append(validation_accuracy)
             acc_x.append(iterations)
             print("EPOCH {} ({} iterations)...".format(i + 1, iterations))
@@ -343,8 +264,6 @@ if training:
     ax2.set_title("Accuracy")
     plt.show()
 
-from PIL import Image
-import os
 image_files = ['example_signs/' + image_file for image_file in os.listdir('example_signs')]
 images = []
 for image_file in image_files:
@@ -388,9 +307,8 @@ for i in range(9):
         prob = "%.1f" % (best_logits[0][i][j] * 100)
         txt = txt + logit + ": " + signs_dict[logit] + ' (' + prob + '%)\n'
         if j < 3:
-            idx = np.where(y_train_aug == best_logits[1][i][j])[0][20]
-            print(idx, X_train[idx].shape)
-            axes[1][j].imshow(X_train_aug[idx])
+            idx = np.where(y_train == best_logits[1][i][j])[0][20]
+            axes[1][j].imshow(X_train_norm[idx])
     f.text(0.4, 0.9, txt, fontsize=10, va='top', wrap = True, bbox={'facecolor':'None'})
     plt.show()
 
@@ -426,4 +344,4 @@ def outputFeatureMap(image_input, tf_activation, activation_min=-1, activation_m
                 plt.imshow(activation[0,:,:, featuremap], interpolation="nearest", cmap="gray")
         plt.show()
 
-# outputFeatureMap(np.expand_dims(images[5], axis=0), act_layer, activation_min=-1, activation_max=-1 ,plt_num=1)
+outputFeatureMap(np.expand_dims(images[5], axis=0), act_layer, activation_min=-1, activation_max=-1 ,plt_num=1)
